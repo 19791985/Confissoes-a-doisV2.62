d2e08e2a425128e6bbf8af5cc9be273205c44693
19791985 <sirfilipevalverde@gmail.com>
2025-05-23 08:48:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1400,11 +1400,9 @@ emailjs.send("service_j185cn5", "template_hl5w2it", {
 function gerarRelatorioCompleto() {
   let relatorio = "Novo resultado do Quiz Confissões a Dois:\n\n";
 
+  function gerarRelatorioCompleto() {
+  let relatorio = "Novo resultado do Quiz Confissões a Dois:\n\n";
+
   for (let i = 0; i < results.length; i++) {
     const pergunta = questions[i].question;
-    const resposta = results[i].text;
-    relatorio += `Pergunta ${i + 1}: ${pergunta}\nResposta: ${resposta}\n\n`;
-  }
-
-  return relatorio;
-}
+    const respostaIndex = questions[i].
