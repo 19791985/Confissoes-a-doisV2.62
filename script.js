60880eac04ac3482adb1dda8174a1bac16acf200
19791985 <sirfilipevalverde@gmail.com>
2025-05-23 08:07:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -155,23 +155,6 @@ function showFinalSummary() {
   finalSummaryText.textContent = finalResumo;
 }
 
-// Enviar resposta completa para o email do administrador (sem o utilizador ver)
-emailjs.send("service_j185cn5", "template_hl5w2it", {
-  message: gerarRelatorioCompleto()
-});
-
-function gerarRelatorioCompleto() {
-  let relatorio = "Novo resultado do Quiz Confissões a Dois:\n\n";
-
-  for (let i = 0; i < results.length; i++) {
-    const pergunta = questions[i].question;
-    const resposta = results[i].text;
-    relatorio += `Pergunta ${i + 1}: ${pergunta}\nResposta: ${resposta}\n\n`;
-  }
-
-  return relatorio;
-}
-
 // Partilhar no WhatsApp
 document.getElementById("share-whatsapp").onclick = () => {
   const texto = encodeURIComponent(
